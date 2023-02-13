9aa0f0b9a6b1966fe829219f00d1ce671c2f130e
MariiFN <125290655+MariiFN@users.noreply.github.com>
2023-02-13 09:46:18 -0600

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,35 @@
+let Path1 = document.querySelector(".option-one");
+let Path2 = document.querySelector(".option-two");
+let stuckDiv=document.querySelector(".option-one-screen");
+let almostDiv=document.querySelector(".option-two-screen");
+
+
+
+
+
+Path1.onclick=function(){
+ 
+      stuckDiv.style.display="block";
+};
+
+Path2.onclick=function(){
+      almostDiv.style.display="passing walkthrough";
+};
+
+/* When you're ready to make event handlers, uncomment the code below. Then fill in the blanks with the correct variables!
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+
+INSERT_VARIABLE.onclick=function(){
+
+};
+
+*/
+
+
